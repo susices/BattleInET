996c868cc189345f6eb3ba5ae94ece1908b97961
wuwenchao <susices@hotmail.com>
2021-11-22 10:36:42 +0800

    Initial commit

---
+++ ET-master/Unity/Assets/ModelView/Demo/Entry.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace ET
+{
+	public class MonoEntry : IEntry
+	{
+		public void Start()
+		{
+			try
+			{
+				string[] assemblyNames = { "Unity.Model.dll", "Unity.Hotfix.dll", "Unity.ModelView.dll", "Unity.HotfixView.dll" };
+				
+				foreach (Assembly assembly in AppDomain.CurrentDomain.GetAssemblies())
+				{
+					string assemblyName = $"{assembly.GetName().Name}.dll";
+					if (!assemblyNames.Contains(assemblyName))
+					{
+						continue;
+					}
+					Game.EventSystem.Add(assembly);
+				}
+				
+				ProtobufHelper.Init();
+				
+				Game.Options = new Options();
+				
+				Game.EventSystem.Publish(new EventType.AppStart()).Coroutine();
+			}
+			catch (Exception e)
+			{
+				Log.Error(e);
+			}
+		}
+
+		public void Update()
+		{
+			ThreadSynchronizationContext.Instance.Update();
+			Game.EventSystem.Update();
+		}
+
+		public void LateUpdate()
+		{
+			Game.EventSystem.LateUpdate();
+		}
+
+		public void OnApplicationQuit()
+		{
+			Game.Close();
+		}
+	}
+}
---
+++ ET-master/Unity/Assets/Mono/MonoBehaviour/Init.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Reflection;
+using System.Threading;
+using UnityEngine;
+
+namespace ET
+{
+	public interface IEntry
+	{
+		void Start();
+		void Update();
+		void LateUpdate();
+		void OnApplicationQuit();
+	}
+	
+	public class Init: MonoBehaviour
+	{
+		private IEntry entry;
+		
+		private void Awake()
+		{
+			SynchronizationContext.SetSynchronizationContext(ThreadSynchronizationContext.Instance);
+			
+			DontDestroyOnLoad(gameObject);
+
+			Assembly modelAssembly = null;
+
+			UnityEngine.Debug.Log("unity editor mode!");
+			foreach (Assembly assembly in AppDomain.CurrentDomain.GetAssemblies())
+			{
+				string assemblyName = $"{assembly.GetName().Name}.dll";
+				if (assemblyName != "Unity.ModelView.dll")
+				{
+					continue;
+				}
+
+				modelAssembly = assembly;
+				break;
+			}
+
+			Type initType = modelAssembly.GetType("ET.MonoEntry");
+			this.entry = Activator.CreateInstance(initType) as IEntry;
+		}
+
+		private void Start()
+		{
+			this.entry.Start();
+		}
+
+		private void Update()
+		{
+			this.entry.Update();
+		}
+
+		private void LateUpdate()
+		{
+			this.entry.LateUpdate();
+		}
+
+		private void OnApplicationQuit()
+		{
+			this.entry.OnApplicationQuit();
+		}
+	}
+}
